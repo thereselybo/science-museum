29fda3d337af56a30514da17bb08eedbda90e334
Therese Lybo <therese.lybo@gmail.com>
2020-06-03 13:59:49 +0200

    Formatted and cleaned up code

--- js/fadein.js
+++ js/fadein.js
@@ -1,14 +1,11 @@
-const spinner = document.querySelector("#spinner")
+const spinner = document.querySelector("#spinner");
 const allElements = document.querySelectorAll(".invisible");
 
-window.onload = function() {
-    spinner.style.display = "none";
-    spinner.style.transition = "all ease-in .5s;"
-    allElements.forEach(function(element) {
-        element.classList.add("fade_in");
-        element.classList.remove("invisible");
-    })
-}
-
-// opacity: 1;
-// transition: all ease-in .8s;
+window.onload = function () {
+  spinner.style.display = "none";
+  spinner.style.transition = "all ease-in .5s;";
+  allElements.forEach(function (element) {
+    element.classList.add("fade_in");
+    element.classList.remove("invisible");
+  });
+};
--- js/nav.js
+++ js/nav.js
@@ -3,29 +3,29 @@ const hamburger = document.querySelector("#hamburger");
 const cross = document.querySelector("#cross");
 
 hamburger.addEventListener("click", function openNav() {
-    hamburger.style.display = "none";
-    cross.style.display = "block";
-    nav.style.height = "100vh";
+  hamburger.style.display = "none";
+  cross.style.display = "block";
+  nav.style.height = "100vh";
 });
 
 cross.addEventListener("click", function closeNav() {
-    cross.style.display = "none";
-    hamburger.style.display = "block";
-    nav.style.height = 0;
+  cross.style.display = "none";
+  hamburger.style.display = "block";
+  nav.style.height = 0;
 });
 
 const breakpoint = window.matchMedia("(min-width: 1024px)");
 
 function checkWindowSize(breakpoint) {
-    if(breakpoint.matches) {
-        nav.style.height = "60px";
-        hamburger.style.display = "none";
-        cross.style.display = "none";
-    } else {
-        nav.style.height = 0;
-        hamburger.style.display = "block";
-    }
+  if (breakpoint.matches) {
+    nav.style.height = "60px";
+    hamburger.style.display = "none";
+    cross.style.display = "none";
+  } else {
+    nav.style.height = 0;
+    hamburger.style.display = "block";
+  }
 }
 
 checkWindowSize(breakpoint);
-breakpoint.addListener(checkWindowSize)
+breakpoint.addListener(checkWindowSize);
