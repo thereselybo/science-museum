bbdab668e6fc294c0bb7109a20a4e0205f299c98
Therese Lybo <therese.lybo@gmail.com>
2020-06-03 13:44:10 +0200

    Loading spinner

---
+++ js/fadein.js
@@ -0,0 +1,14 @@
+const spinner = document.querySelector("#spinner")
+const allElements = document.querySelectorAll(".invisible");
+
+window.onload = function() {
+    spinner.style.display = "none";
+    spinner.style.transition = "all ease-in .5s;"
+    allElements.forEach(function(element) {
+        element.classList.add("fade_in");
+        element.classList.remove("invisible");
+    })
+}
+
+// opacity: 1;
+// transition: all ease-in .8s;
